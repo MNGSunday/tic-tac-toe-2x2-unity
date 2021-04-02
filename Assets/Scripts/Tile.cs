3662aa07e2b065b1c21b02940e1f0fed9563266c
Marc Domingo <mdsunday01@gmail.com>
2021-04-02 10:44:36 -0700

    First Attempt at Project 6, ask Professor Boyd about OnMouseUp issue

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,6 +7,8 @@ public class Tile : MonoBehaviour
     public TileManager tileManager;
     public TileManager.Owner owner;
 
+    public Sprite[] ownerSprite = new Sprite[3];
+
     private void OnMouseUp()
     {
         // Check for current player that is claiming ownership of this space
@@ -14,9 +16,11 @@ public class Tile : MonoBehaviour
 
         // Set the sprite color to represent the owner (Sword = Blue, Shield = Red)
         if (owner == TileManager.Owner.Sword)
-            this.GetComponent<SpriteRenderer>().color = Color.blue;
+            this.GetComponent<SpriteRenderer>().sprite = ownerSprite[1];
         else if (owner == TileManager.Owner.Shield)
-            this.GetComponent<SpriteRenderer>().color = Color.red;
+            this.GetComponent<SpriteRenderer>().sprite = ownerSprite[2];
+        else if (owner == TileManager.Owner.None)
+            this.GetComponent<SpriteRenderer>().sprite = ownerSprite[0];
 
         // Update to the next player in rotation
         tileManager.ChangePlayer();
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TileManager : MonoBehaviour
 {
     public Owner CurrentPlayer;
-    public Tile[] Tiles = new Tile[4];
+    public Tile[] Tiles = new Tile[9];
+
+    public GameObject resetButton;
+    public GameObject quitButton;
+
+    public Text SwordPlayerScore;
+    public Text ShieldPlayerScore;
+
+    private int SwordScore = 0;
+    private int ShieldScore = 0;
 
     public enum Owner
     {
@@ -19,6 +29,8 @@ public class TileManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        resetButton.SetActive(false);
+        quitButton.SetActive(false);
         won = false;
         CurrentPlayer = Owner.Sword;
     }
@@ -26,7 +38,24 @@ public class TileManager : MonoBehaviour
     public void ChangePlayer()
     {
         if (CheckForWinner())
+        {
+            resetButton.SetActive(true);
+            quitButton.SetActive(true);
+
+            if (CurrentPlayer == Owner.Sword)
+            {
+                SwordScore += 1;
+                SwordPlayerScore.text = "Sword Player Score: " + SwordScore;
+            }
+
+            else if (CurrentPlayer == Owner.Shield)
+            {
+                ShieldScore += 1;
+                ShieldPlayerScore.text = "Shield Player Score: " + ShieldScore;
+            }
+
             return;
+        }
 
         if (CurrentPlayer == Owner.Sword)
             CurrentPlayer = Owner.Shield;
@@ -36,15 +65,24 @@ public class TileManager : MonoBehaviour
 
     public bool CheckForWinner()
     {
-        if (Tiles[0].owner == CurrentPlayer && Tiles[1].owner == CurrentPlayer)
+        // Horizontal Victories
+        if (Tiles[0].owner == CurrentPlayer && Tiles[1].owner == CurrentPlayer && Tiles[2].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[0].owner == CurrentPlayer && Tiles[2].owner == CurrentPlayer)
+        else if (Tiles[3].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[5].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[0].owner == CurrentPlayer && Tiles[3].owner == CurrentPlayer)
+        else if (Tiles[6].owner == CurrentPlayer && Tiles[7].owner == CurrentPlayer && Tiles[8].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[1].owner == CurrentPlayer && Tiles[2].owner == CurrentPlayer)
+        // Vertical Victories
+        else if (Tiles[0].owner == CurrentPlayer && Tiles[3].owner == CurrentPlayer && Tiles[6].owner == CurrentPlayer)
             won = true;
-        else if (Tiles[1].owner == CurrentPlayer && Tiles[3].owner == CurrentPlayer)
+        else if (Tiles[1].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[7].owner == CurrentPlayer)
+            won = true;
+        else if (Tiles[2].owner == CurrentPlayer && Tiles[7].owner == CurrentPlayer && Tiles[8].owner == CurrentPlayer)
+            won = true;
+        // Diagonal Victories
+        else if (Tiles[0].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[8].owner == CurrentPlayer)
+            won = true;
+        else if (Tiles[2].owner == CurrentPlayer && Tiles[4].owner == CurrentPlayer && Tiles[6].owner == CurrentPlayer)
             won = true;
 
         if (won)
